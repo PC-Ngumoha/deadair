76d92dec2aa9be287be00d204846e83eff353a80
PC-Ngumoha <pcngumoha@gmail.com>
2026-08-12 15:17:17 +0100

    CallToAction section done.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CallToAction } from "@/sections/CallToAction";
 import { Features } from "@/sections/Features";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
@@ -16,6 +17,7 @@ export default function Home() {
       <Features />
       <Testimonial />
       <Pricing />
+      <CallToAction />
     </main>
   );
 }
--- sections/CallToAction.tsx
+++ sections/CallToAction.tsx
@@ -1,3 +1,24 @@
 export function CallToAction() {
-  return null;
+  return (
+    <div className="flex items-center justify-center h-[60vh] py-4 px-2 my-7.5 bg-wood-brown">
+      <div
+        className="bg-wood-brown rounded-2xl shadow-lg p-8 w-[80%] h-[80%] text-center flex
+      flex-col justify-center gap-4 border border-highlight-brown"
+      >
+        <div className="flex flex-col gap-4 items-center text-center">
+          <h2 className="text-5xl font-bold mb-3 text-white font-grotesk tracking-tighter">
+            Stop wasting hours on technicalities.
+          </h2>
+          <p className="section-subtitle">
+            Join 5000+ podcasters who spend more time talking and less time
+            cutting. Start your free trial today.
+          </p>
+        </div>
+        <div className="w-[60%] mx-auto flex justify-center gap-3">
+          <button className="btn-xl btn-filled">Try Deadair for free</button>
+          <button className="btn-xl btn-text">Explore the Docs</button>
+        </div>
+      </div>
+    </div>
+  );
 }
